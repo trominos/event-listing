dd613cb4acb237016eae1d87cff631feca284005
JosephGeorge1234 <jg662@pvsd.org>
2020-05-12 18:05:17 -0400

    initial commit

---
+++ src/test/java/com/mvp/eventlisting/EventListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvp.eventlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
